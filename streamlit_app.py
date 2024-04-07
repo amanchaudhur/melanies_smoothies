6071ee16d04645db0570dfd645f08e2402e028d3
Aman Chaudhur <104425157+amanchaudhur@users.noreply.github.com>
2024-04-07 15:44:22 +0530

    Update streamlit_app.py
    in json format

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,6 +44,6 @@ if ingredients_list:
 
     
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+st.text(fruityvice_response.json())
 
 
